15666157bed27f245ad6c3fd643b9ebc7a115487
Khizar29 <s.khizarli03@gmail.com>
2025-03-12 13:34:27 +0500

    Updated POM, tests

--- src/test/java/com/paysyslabs/employment_management/EmploymentManagementApplicationTests.java
+++ src/test/java/com/paysyslabs/employment_management/EmploymentManagementApplicationTests.java
@@ -2,12 +2,13 @@ package com.paysyslabs.employment_management;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
 class EmploymentManagementApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
